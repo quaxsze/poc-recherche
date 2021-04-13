58759c0313ab3e950279ded5d21361034c474ca7
quaxsze <service@plum.eu.com>
2021-04-13 16:35:05 +0200

    update ES dsn

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,6 @@ from config import Config
 def create_app(config_class=Config):
     app = Flask(__name__)
 
-    import os
-    os.environ['ELASTICSEARCH_URL']
-
     app.config.from_object(config_class)
     app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
         if app.config['ELASTICSEARCH_URL'] else None
